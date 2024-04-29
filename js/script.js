8bee44b94d4a81680a5ecb5e6c9ac9bd1fde8394
matteocenci <matteocenci01@gmail.com>
2024-04-29 16:31:30 +0200

    feat: printing emails in doc

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ createApp({
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
                     const mail = response.data.response
                     console.log(mail);
+                    this.mailArray.push(mail)
                 });
                 
             }
